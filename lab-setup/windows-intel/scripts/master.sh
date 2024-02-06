a796f8e6fef716e94e55d4555b6be14fc7213860
rmkprabhu <manikandaprabhuravi@gmail.com>
2024-02-06 16:49:32 +0530

    Update master.sh - commented calico installation lines

--- lab-setup/windows-intel/scripts/master.sh
+++ lab-setup/windows-intel/scripts/master.sh
@@ -36,9 +36,9 @@ kubeadm token create --print-join-command > $config_path/join.sh
 
 # Install Calico Network Plugin
 
-curl https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml -O
+#curl https://raw.githubusercontent.com/projectcalico/calico/v${CALICO_VERSION}/manifests/calico.yaml -O
 
-kubectl apply -f calico.yaml
+#kubectl apply -f calico.yaml
 
 sudo -i -u vagrant bash << EOF
 whoami
